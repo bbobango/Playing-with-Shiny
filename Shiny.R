bff68c7d8773026b851dc6370890482e975eb466
bbobango <bpbechev@gmail.com>
2021-03-06 11:19:33 +0100

    Basic Shiny script

---
+++ Shiny.R
@@ -0,0 +1,35 @@
+#Preliminaries
+rm(list=ls())
+library(utils)
+library(tidyverse)
+library(shiny)
+
+#Getting the data
+
+#Shiny template
+ui <- fluidPage(
+  titlePanel('Welcome to my Shiny app'),
+  
+  sidebarLayout(
+    sidebarPanel(
+  #Input() functions
+      sliderInput(inputId = 'num',
+                  label = 'Choose a number',
+                  value = 25, min = 1, max = 100),
+      #Output() functions
+    ),
+    mainPanel(
+      plotOutput('hist')
+    )
+  )
+)
+
+server <- function(input, output){
+  output$hist <- renderPlot({
+    title <- "100 random normal values"
+    hist(rnorm(input$num))
+  })
+  }
+
+shinyApp(ui = ui, server = server)
+
